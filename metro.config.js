4d35c029f8647c766dfc6b0033f06a60bfef97c2
Joseph <josephjordanuk@icloud.com>
2025-08-22 19:58:15 +0100

    chore: add metro + tsconfig tweaks for Firebase RN persistence

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+// Metro config for Expo + Firebase SDK compatibility
+// lastUpdated 2025-08-22
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+config.resolver.sourceExts.push('cjs');
+config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config;
